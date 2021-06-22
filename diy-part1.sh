363020e08e66c466387d57be20fd8c727797d458
qwsxyear <45333782+qwsxyear@users.noreply.github.com>
2021-06-23 00:19:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,13 +26,7 @@ rm -rf package/lean/luci-theme-material
 rm -rf package/lean/luci-theme-netgear
 
 # 下载主题luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon_bing
-
-# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon_bing/g' feeds/luci/collections/luci/Makefile
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # passwall插件
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
-
-# 主题argon配置插件
-git clone https://github.com/jerrykuku/luci-theme-argon/releases/download/v1.7.2/luci-app-argon-config_0.9-20210309_all.ipk.git package/luci-app-argon-config
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
