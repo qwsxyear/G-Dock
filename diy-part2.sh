9aae6a793fd71b6e238741eb5abfa1ecb1baa696
qwsxyear <45333782+qwsxyear@users.noreply.github.com>
2022-02-13 19:06:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# 修改登录IP
+# 修改登录IP为192.168.2.1
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
