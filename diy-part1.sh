6deddd1dc002dcf82d828e40b5bd2af7e9212b4d
qwsxyear <45333782+qwsxyear@users.noreply.github.com>
2022-02-13 19:05:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -30,3 +30,6 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/lu
 
 # passwall插件
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/lean/luci-app-passwall
+
+# 网络唤醒++
+git clone https://github.com/sundaqiang/openwrt-packages/tree/master/luci-app-wolplus.git package/lean/luci-app-wolplus
